e26e849dafb46c072751e46b15ca753b0db38bd6
lsfoo <lsfoo@qq.com>
2018-11-02 15:50:39 +0800

    kdk

--- vue-el/src/App.vue
+++ vue-el/src/App.vue
@@ -1,68 +1,13 @@
 <template>
   <div id="app">
     <el-container>
-      <el-aside width="240px" class="sidebar">
-        <el-col :span="24">
-          <h2 style="margin:50px">面板</h2>
-          <div>
-
-          <el-input   placeholder="搜索菜单" ></el-input>
-          </div>
-          <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"  >
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
-              </template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-              <i class="el-icon-document"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i class="el-icon-setting"></i>
-              <span slot="title">导航四</span>
-            </el-menu-item>
-          </el-menu>
-        </el-col>
-
-      </el-aside>
+      <el-header>
+        <c-navbar />
+      </el-header>
       <el-container>
-        <el-header>
-          <el-menu :default-active="activeIndex2"  mode="horizontal" @select="handleSelect" >
-            <el-menu-item index="1">处理中心</el-menu-item>
-            <el-submenu index="2">
-              <template slot="title">我的工作台</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-              <el-menu-item index="2-3">选项3</el-menu-item>
-              <el-submenu index="2-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="2-4-1">选项1</el-menu-item>
-                <el-menu-item index="2-4-2">选项2</el-menu-item>
-                <el-menu-item index="2-4-3">选项3</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="3" disabled>消息中心</el-menu-item>
-            <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-          </el-menu>
-        </el-header>
+        <el-aside width="240px" class="sidebar">
+          <c-sidebar />
+        </el-aside>
         <el-main>
           <router-view />
         </el-main>
@@ -71,7 +16,14 @@
   </div>
 </template>
 <script>
-export default {};
+import Sidebar from "@/components/common/sidebar";
+import Navbar from "@/components/common/navbar";
+export default {
+  components: {
+    "c-sidebar": Sidebar,
+    "c-navbar": Navbar
+  }
+};
 </script>
 
 <style>
@@ -80,37 +32,42 @@ html {
   height: 100%;
   padding: 0px;
   margin: 0px;
+  color: #45474c;
 }
 #app {
   height: 100%;
 }
 .el-header {
-  padding: 0 0 ;
-  
+  padding: 0 0;
 }
-.el-menu{
+.el-menu {
   border: 0;
-
 }
 .el-aside {
-  background-color: #f9f9f9;
-  border-right:1px solid #eee;
   height: 100%;
+
+box-shadow: 6px 0 6px rgba(0,0,0,.1)
 }
 .el-container {
   height: 100%;
 }
-.el-main{
+.el-main {
   overflow-y: auto;
+  padding-left: 40px;
+  padding-right: 40px;
 }
 /*定义滚动条高宽及背景 高宽分别对应横竖滚动条的尺寸*/
 ::-webkit-scrollbar {
-  width: 8px;
-  height: 8px;
+  width: 5px;
+  height: 5px;
+}
+/*定义滚动条轨道 内阴影+圆角*/
+::-webkit-scrollbar-track {
 }
 
 /*定义滑块 内阴影+圆角*/
 ::-webkit-scrollbar-thumb {
-  background-color: #eee;
+  border-radius: 2px;
+  background-color: #666666;
 }
 </style>
---
+++ vue-el/src/components/common/navbar.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <el-menu class="shadow" :default-active="activeIndex2"  mode="horizontal" @select="handleSelect"  background-color="#1976d2" text-color="#fff" active-text-color="#ffffcc">
+
+        <el-menu-item  >  <span style="font-size:22px" > 控制面板</span></el-menu-item>
+        <el-submenu index="1" style="float:right">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2" style="float:right" >消息中心</el-menu-item>
+    </el-menu>
+</template>
+<style>
+.shadow{
+    box-shadow: 0 2px 5px 0 rgba(0,0,0,.3)
+
+}
+.el-menu.el-menu--horizontal{
+    border: 0px;
+}
+
+
+</style>
+
